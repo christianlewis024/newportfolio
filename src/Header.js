b1b15fc3101e42537490e21ee5598296c70ebdc6
Christian Lewis <christianlewis024@gmail.com>
2020-10-23 15:23:53 -0700

    header styled for now

--- src/Header.js
+++ src/Header.js
@@ -10,12 +10,17 @@ import MailIcon from '@material-ui/icons/Mail';
 function Header() {
     return (
         <div className="header">
-           <Avatar className="header__avatar" alt="christian" fontSize="large" src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/32760939_10155490568442393_4979765686357196800_n.jpg?_nc_cat=105&ccb=2&_nc_sid=8bfeb9&_nc_ohc=Z6ubzEUy85gAX_Tilqn&_nc_ht=scontent-lax3-1.xx&oh=3598d200fac3d620a7e851d32aec641a&oe=5FB7A593"/>
-        <div className="header__name">
+          
+        <div className="header__left">
+        <Avatar className="header__avatar" alt="christian" fontSize="large" src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/32760939_10155490568442393_4979765686357196800_n.jpg?_nc_cat=105&ccb=2&_nc_sid=8bfeb9&_nc_ohc=Z6ubzEUy85gAX_Tilqn&_nc_ht=scontent-lax3-1.xx&oh=3598d200fac3d620a7e851d32aec641a&oe=5FB7A593"/> 
+            
+            <div className="header__name">
             <h2>Christian Lewis </h2>
             <h5><LocationIcon/>Orange County, California</h5>
             <h3><CheckMarkIcon/>Open to work</h3>
             <h3><CheckMarkIcon/>Willing to relocate</h3>
+            </div>
+
         </div>
         <div className="header__intro">
             <h2> Full Stack Web Developer with a passion for React and Firebase </h2>
@@ -36,8 +41,8 @@ function Header() {
               </a>
               </div>
               <div className="header__contactBottom">
-             
-              <a href = "mailto: christianlewis024@gmail.com"><MailIcon/>ChristianLewis024@gmail.com</a>
+              <MailIcon/>
+              <a href = "mailto: christianlewis024@gmail.com">ChristianLewis024@gmail.com</a>
               </div>
         </div>
         </div>
